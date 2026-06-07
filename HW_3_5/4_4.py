250d0d6a1b86f783fd0baaee1604a0a43b69f7c6
Son12232232 <157476704+Son12232232@users.noreply.github.com>
2026-06-07 12:48:10 +0300

    Create 4_4.py

---
+++ HW_3_5/4_4.py
@@ -0,0 +1,22 @@
+import math
+
+
+def f(x):
+    return math.sin(x) - x / 3
+
+
+left = 1.6
+right = 3.0
+
+# Метод бісекції
+for _ in range(100):
+    middle = (left + right) / 2
+
+    if f(left) * f(middle) <= 0:
+        right = middle
+    else:
+        left = middle
+
+x = (left + right) / 2
+
+print(f"x = {x:.10f}")
