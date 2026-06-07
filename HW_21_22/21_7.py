1f1c1132d869dcd459cc4f42391830e37ff10c3e
Son12232232 <157476704+Son12232232@users.noreply.github.com>
2026-06-07 23:41:10 +0300

    Create 21_7.py

---
+++ HW_21_22/21_7.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+
+degree = [0] * n
+
+for _ in range(m):
+    u, v = map(int, input().split())
+
+    degree[u - 1] += 1
+    degree[v - 1] += 1
+
+for value in degree:
+    print(value)
